d4f8624b1a29d65d93e4e42c446960d4529d2032
pawelEr <rosz.pawel@gmail.com>
2011-08-27 22:51:30 +0200

    porządki

---
+++ symulator8051/Commands/ICommand.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace symulator8051
+{
+    interface ICommand
+    {
+        byte Cycles {get;}
+        void execute();
+    }
+}
