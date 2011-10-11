68504876b28343bbfcf64ff413e1c51334866e19
Neo <devnull@localhost>
2011-10-11 17:26:47 +0200

    Dwie instrukcje gdzie w MUL brak zmiany wskażników

---
+++ symulator8051/Commands/Div.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace symulator8051.Commands
+{
+    class x84 : ICommand //DIV AB
+    {
+        I8051 i;
+        private byte cycles = 4;
+        public byte Cycles
+        {
+            get { return cycles; }
+        }
+        private ushort bytes = 1;
+        public ushort Bytes
+        {
+            get { return bytes; }
+        }
+        public x84(I8051 i)
+        {
+            this.i = i;
+        }
+        private byte temp;
+        public void execute()
+        {
+            temp = (byte)(i.ACC % i.B);
+            i.ACC = (byte)(i.ACC / i.B);
+            i.B = temp;
+        }
+    }
+}
---
+++ symulator8051/Commands/Mul.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace symulator8051.Commands
+{
+    class xA4 : ICommand //MUL AB
+    {
+        I8051 i;
+        private byte cycles = 4;
+        public byte Cycles
+        {
+            get { return cycles; }
+        }
+        private ushort bytes = 1;
+        public ushort Bytes
+        {
+            get { return bytes; }
+        }
+        public xA4(I8051 i)
+        {
+            this.i = i;
+        }
+        private byte temp;
+        public void execute()
+        {
+            if (i.ACC * i.B > 255)
+            {
+
+            }
+            if (i.ACC * i.B < 255)
+            {
+
+            }
+            temp = Convert.ToByte((i.ACC * i.B) % 256 );
+            i.ACC = Convert.ToByte((i.ACC * i.B) / 256);
+            i.B = temp;
+        }
+    }
+}
