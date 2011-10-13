2b3a01b449f9eef5d819445283034fed40d43ab7
Neo <devnull@localhost>
2011-10-13 18:24:06 +0200

    SWAP

---
+++ symulator8051/Commands/Swap.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace symulator8051.Commands
+{
+    class xC4 : ICommand //SWAP A
+    {
+        I8051 i;
+        private byte cycles = 1;
+        public byte Cycles
+        {
+            get { return cycles; }
+        }
+        private ushort bytes = 1;
+        public ushort Bytes
+        {
+            get { return bytes; }
+        }
+        public xC4(I8051 i)
+        {
+            this.i = i;
+        }
+        public void execute()
+        {
+            i.ACC = (byte)((i.ACC << 4) | (i.ACC >> 4));
+        }
+    }
+}
