a007b772b9737496ca3e7ade75a91533ffac4732
Neo <devnull@localhost>
2011-10-13 13:56:54 +0200

    Zmieniony ADD + obsługa flag

--- symulator8051/Commands/Add.cs
+++ symulator8051/Commands/Add.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using ByteExtensionMethods;
 
 namespace symulator8051.Commands
 {
@@ -24,9 +25,26 @@ namespace symulator8051.Commands
             this.i = i;
             this.arg = arg;
         }
+        private UInt16 temp;
         public void execute()
         {
-            i.ACC = Convert.ToByte( i.ACC + i.SFR[arg]);
+            temp = (UInt16)(i.ACC + i.SFR[arg]);
+            ByteHelper.clrBit(7, 0);
+            ByteHelper.clrBit(2, 0);
+            ByteHelper.clrBit(6, 0);
+            if ((i.ACC & 0x0f) + (i.SFR[arg] & 0x0f) > 0x0f)
+             ByteHelper.setBit(6,1);
+            if ((i.ACC & 0x7f) + (i.SFR[arg] & 0x7f) > 0x7f)
+                ByteHelper.setBit(2, 1);
+            if (temp > 0xff)
+            {
+                ByteHelper.setBit(7, 1);
+                if ( ByteHelper.chkBit(2, 0))
+                    ByteHelper.setBit(2,1);
+                else
+                    ByteHelper.setBit(2,0);
+            }
+            i.ACC=(byte)temp;
         }
     }
     class x25 : ICommand //ADD A, iram addr
@@ -48,9 +66,26 @@ namespace symulator8051.Commands
             this.i = i;
             this.arg = arg;
         }
+        private UInt16 temp;
         public void execute()
         {
-            i.ACC = Convert.ToByte(i.ACC + arg);
+            temp = (UInt16)(i.ACC + arg);
+            ByteHelper.clrBit(7, 0);
+            ByteHelper.clrBit(2, 0);
+            ByteHelper.clrBit(6, 0);
+            if ((i.ACC & 0x0f) + (arg & 0x0f) > 0x0f)
+                ByteHelper.setBit(6, 1);
+            if ((i.ACC & 0x7f) + (arg & 0x7f) > 0x7f)
+                ByteHelper.setBit(2, 1);
+            if (temp > 0xff)
+            {
+                ByteHelper.setBit(7, 1);
+                if (ByteHelper.chkBit(2, 0))
+                    ByteHelper.setBit(2, 1);
+                else
+                    ByteHelper.setBit(2, 0);
+            }
+            i.ACC = (byte)temp;
         }
     }
     class x26 : ICommand //ADD A, @R0
@@ -71,9 +106,26 @@ namespace symulator8051.Commands
         {
             this.i = i;
         }
+        private UInt16 temp;
         public void execute()
         {
-            i.ACC = Convert.ToByte(i.ACC + i.EXT_RAM[i.R0]);
+            temp = (UInt16)(i.ACC + i.EXT_RAM[i.R0]);
+            ByteHelper.clrBit(7, 0);
+            ByteHelper.clrBit(2, 0);
+            ByteHelper.clrBit(6, 0);
+            if ((i.ACC & 0x0f) + (i.EXT_RAM[i.R0] & 0x0f) > 0x0f)
+                ByteHelper.setBit(6, 1);
+            if ((i.ACC & 0x7f) + (i.EXT_RAM[i.R0] & 0x7f) > 0x7f)
+                ByteHelper.setBit(2, 1);
+            if (temp > 0xff)
+            {
+                ByteHelper.setBit(7, 1);
+                if (ByteHelper.chkBit(2, 0))
+                    ByteHelper.setBit(2, 1);
+                else
+                    ByteHelper.setBit(2, 0);
+            }
+            i.ACC = (byte)temp;
         }
     }
 
@@ -95,9 +147,26 @@ namespace symulator8051.Commands
         {
             this.i = i;
         }
+        private UInt16 temp;
         public void execute()
         {
-            i.ACC = Convert.ToByte(i.ACC + i.EXT_RAM[i.R1]);
+            temp = (UInt16)(i.ACC + i.EXT_RAM[i.R1]);
+            ByteHelper.clrBit(7, 0);
+            ByteHelper.clrBit(2, 0);
+            ByteHelper.clrBit(6, 0);
+            if ((i.ACC & 0x0f) + (i.EXT_RAM[i.R1] & 0x0f) > 0x0f)
+                ByteHelper.setBit(6, 1);
+            if ((i.ACC & 0x7f) + (i.EXT_RAM[i.R1] & 0x7f) > 0x7f)
+                ByteHelper.setBit(2, 1);
+            if (temp > 0xff)
+            {
+                ByteHelper.setBit(7, 1);
+                if (ByteHelper.chkBit(2, 0))
+                    ByteHelper.setBit(2, 1);
+                else
+                    ByteHelper.setBit(2, 0);
+            }
+            i.ACC = (byte)temp;
         }
     }
     class x28 : ICommand //ADD A, R0
@@ -118,9 +187,26 @@ namespace symulator8051.Commands
         {
             this.i = i;
         }
+        private UInt16 temp;
         public void execute()
         {
-            i.ACC = Convert.ToByte(i.ACC + i.R0);
+            temp = (UInt16)(i.ACC + i.R0);
+            ByteHelper.clrBit(7, 0);
+            ByteHelper.clrBit(2, 0);
+            ByteHelper.clrBit(6, 0);
+            if ((i.ACC & 0x0f) + (i.R0 & 0x0f) > 0x0f)
+                ByteHelper.setBit(6, 1);
+            if ((i.ACC & 0x7f) + (i.R0 & 0x7f) > 0x7f)
+                ByteHelper.setBit(2, 1);
+            if (temp > 0xff)
+            {
+                ByteHelper.setBit(7, 1);
+                if (ByteHelper.chkBit(2, 0))
+                    ByteHelper.setBit(2, 1);
+                else
+                    ByteHelper.setBit(2, 0);
+            }
+            i.ACC = (byte)temp;
         }
     }
     class x29 : ICommand //ADD A, R1
@@ -141,9 +227,26 @@ namespace symulator8051.Commands
         {
             this.i = i;
         }
+        private UInt16 temp;
         public void execute()
         {
-            i.ACC = Convert.ToByte(i.ACC + i.R1);
+            temp = (UInt16)(i.ACC + i.R1);
+            ByteHelper.clrBit(7, 0);
+            ByteHelper.clrBit(2, 0);
+            ByteHelper.clrBit(6, 0);
+            if ((i.ACC & 0x0f) + (i.R1 & 0x0f) > 0x0f)
+                ByteHelper.setBit(6, 1);
+            if ((i.ACC & 0x7f) + (i.R1 & 0x7f) > 0x7f)
+                ByteHelper.setBit(2, 1);
+            if (temp > 0xff)
+            {
+                ByteHelper.setBit(7, 1);
+                if (ByteHelper.chkBit(2, 0))
+                    ByteHelper.setBit(2, 1);
+                else
+                    ByteHelper.setBit(2, 0);
+            }
+            i.ACC = (byte)temp;
         }
     }
     class x2A : ICommand //ADD A, R2
@@ -164,9 +267,26 @@ namespace symulator8051.Commands
         {
             this.i = i;
         }
+        private UInt16 temp;
         public void execute()
         {
-            i.ACC = Convert.ToByte(i.ACC + i.R2);
+            temp = (UInt16)(i.ACC + i.R2);
+            ByteHelper.clrBit(7, 0);
+            ByteHelper.clrBit(2, 0);
+            ByteHelper.clrBit(6, 0);
+            if ((i.ACC & 0x0f) + (i.R2 & 0x0f) > 0x0f)
+                ByteHelper.setBit(6, 1);
+            if ((i.ACC & 0x7f) + (i.R2 & 0x7f) > 0x7f)
+                ByteHelper.setBit(2, 1);
+            if (temp > 0xff)
+            {
+                ByteHelper.setBit(7, 1);
+                if (ByteHelper.chkBit(2, 0))
+                    ByteHelper.setBit(2, 1);
+                else
+                    ByteHelper.setBit(2, 0);
+            }
+            i.ACC = (byte)temp;
         }
     }
     class x2B : ICommand //ADD A, R3
@@ -187,9 +307,26 @@ namespace symulator8051.Commands
         {
             this.i = i;
         }
+        private UInt16 temp;
         public void execute()
         {
-            i.ACC = Convert.ToByte(i.ACC + i.R3);
+            temp = (UInt16)(i.ACC + i.R3);
+            ByteHelper.clrBit(7, 0);
+            ByteHelper.clrBit(2, 0);
+            ByteHelper.clrBit(6, 0);
+            if ((i.ACC & 0x0f) + (i.R3 & 0x0f) > 0x0f)
+                ByteHelper.setBit(6, 1);
+            if ((i.ACC & 0x7f) + (i.R3 & 0x7f) > 0x7f)
+                ByteHelper.setBit(2, 1);
+            if (temp > 0xff)
+            {
+                ByteHelper.setBit(7, 1);
+                if (ByteHelper.chkBit(2, 0))
+                    ByteHelper.setBit(2, 1);
+                else
+                    ByteHelper.setBit(2, 0);
+            }
+            i.ACC = (byte)temp;
         }
     }
     class x2C : ICommand //ADD A, R4
@@ -210,9 +347,26 @@ namespace symulator8051.Commands
         {
             this.i = i;
         }
+        private UInt16 temp;
         public void execute()
         {
-            i.ACC = Convert.ToByte(i.ACC + i.R4);
+            temp = (UInt16)(i.ACC + i.R4);
+            ByteHelper.clrBit(7, 0);
+            ByteHelper.clrBit(2, 0);
+            ByteHelper.clrBit(6, 0);
+            if ((i.ACC & 0x0f) + (i.R4 & 0x0f) > 0x0f)
+                ByteHelper.setBit(6, 1);
+            if ((i.ACC & 0x7f) + (i.R4 & 0x7f) > 0x7f)
+                ByteHelper.setBit(2, 1);
+            if (temp > 0xff)
+            {
+                ByteHelper.setBit(7, 1);
+                if (ByteHelper.chkBit(2, 0))
+                    ByteHelper.setBit(2, 1);
+                else
+                    ByteHelper.setBit(2, 0);
+            }
+            i.ACC = (byte)temp;
         }
     }
     class x2D : ICommand //ADD A, R5
@@ -233,9 +387,26 @@ namespace symulator8051.Commands
         {
             this.i = i;
         }
+        private UInt16 temp;
         public void execute()
         {
-            i.ACC = Convert.ToByte(i.ACC + i.R5);
+            temp = (UInt16)(i.ACC + i.R5);
+            ByteHelper.clrBit(7, 0);
+            ByteHelper.clrBit(2, 0);
+            ByteHelper.clrBit(6, 0);
+            if ((i.ACC & 0x0f) + (i.R5 & 0x0f) > 0x0f)
+                ByteHelper.setBit(6, 1);
+            if ((i.ACC & 0x7f) + (i.R5 & 0x7f) > 0x7f)
+                ByteHelper.setBit(2, 1);
+            if (temp > 0xff)
+            {
+                ByteHelper.setBit(7, 1);
+                if (ByteHelper.chkBit(2, 0))
+                    ByteHelper.setBit(2, 1);
+                else
+                    ByteHelper.setBit(2, 0);
+            }
+            i.ACC = (byte)temp;
         }
     }
     class x2E : ICommand //ADD A, R6
@@ -256,9 +427,26 @@ namespace symulator8051.Commands
         {
             this.i = i;
         }
+        private UInt16 temp;
         public void execute()
         {
-            i.ACC = Convert.ToByte(i.ACC + i.R6);
+            temp = (UInt16)(i.ACC + i.R6);
+            ByteHelper.clrBit(7, 0);
+            ByteHelper.clrBit(2, 0);
+            ByteHelper.clrBit(6, 0);
+            if ((i.ACC & 0x0f) + (i.R6 & 0x0f) > 0x0f)
+                ByteHelper.setBit(6, 1);
+            if ((i.ACC & 0x7f) + (i.R6 & 0x7f) > 0x7f)
+                ByteHelper.setBit(2, 1);
+            if (temp > 0xff)
+            {
+                ByteHelper.setBit(7, 1);
+                if (ByteHelper.chkBit(2, 0))
+                    ByteHelper.setBit(2, 1);
+                else
+                    ByteHelper.setBit(2, 0);
+            }
+            i.ACC = (byte)temp;
         }
     }
     class x2F : ICommand //ADD A, R7
@@ -279,9 +467,25 @@ namespace symulator8051.Commands
         {
             this.i = i;
         }
+        private UInt16 temp;
         public void execute()
         {
-            i.ACC = Convert.ToByte(i.ACC + i.R7);
-        }
+            temp = (UInt16)(i.ACC + i.R7);
+            ByteHelper.clrBit(7, 0);
+            ByteHelper.clrBit(2, 0);
+            ByteHelper.clrBit(6, 0);
+            if ((i.ACC & 0x0f) + (i.R7 & 0x0f) > 0x0f)
+                ByteHelper.setBit(6, 1);
+            if ((i.ACC & 0x7f) + (i.R7 & 0x7f) > 0x7f)
+                ByteHelper.setBit(2, 1);
+            if (temp > 0xff)
+            {
+                ByteHelper.setBit(7, 1);
+                if (ByteHelper.chkBit(2, 0))
+                    ByteHelper.setBit(2, 1);
+                else
+                    ByteHelper.setBit(2, 0);
+            }
+            i.ACC = (byte)temp;
     }
 }
